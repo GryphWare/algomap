33886126a5b3782e4da40a1c3fd2c69395a7c08e
hygrph <chanhnguyencute12@gmail.com>
2024-08-29 22:14:19 +0700

    change

--- 771.c
+++ 771.c
@@ -1,69 +1,35 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
 
-//lam theo kieu hashmap
-
-//struct chu cai
-typedef struct {
-    char word; //chu cai thuong
-    char word_cap;  //in hoa
-    } hashmap;
-
-//ham diff nay se tra ve true or false neu chu trong user != vs map thi false;
-bool diff (hashmap *Map, char key, int size){
-    //tao loop
-    for (size_t i = 0; i < size; i++){
-        if (key == Map[i].word || key == Map[i].word_cap){
-            return true;
+//da function
+int compare(const char* j, const char* s){
+    
+    //cuz ascii have 128 str?
+    bool False[128] = {false};
+    
+    //in j str -> true (change)
+    for (size_t i = 0; j[i] != '\0'; i++){
+        False[(int)j[i]] = true;
+        }
+    
+    //check if key in s == j    
+    int count = 0;
+    for (size_t i = 0; s[i] != '\0'; i++){
+        if(False[(int)s[i]]){ //true
+            count++;
             }
         }
-        return false;
-    }
-
-//ham tra ve result;
-int result (const char* user, const char* match){
-    //tao hashmap voi size
-    int size = strlen(match);
-    hashmap Map[size];
-    //tao bien ket qua
-    int total = 0;
-    
-    //tao vong lap tao hash map voi thuong va in hoa thong qua match
-    for (int i = 0; i < size; i++){
-        //xet xem co phai in hoa ko
-        if (match[i] == toupper(match[i])){
-            Map[i].word = tolower(match[i]);
-            Map[i].word_cap = match[i];
-            } else {
-                Map[i].word = match[i];
-            Map[i].word_cap = toupper(match[i]);
-                }
-    }
-        //tao xong r xet user vs match;
-        for (int i = 0; i < strlen(user); i++){
-            int addOrNot = diff(Map, user[i],size);
-            //neu no tra 1 thi cong vao add
-            if (addOrNot){
-               total++;
-               }   
-        }
-    
-    return total;
+    return count;
     }
 
 int main(){
     
-    //tao bien cho user nhap
-    char user[] = "Abc";
+    char j[] = "aA";//ran
+    char s[] = "aAABbbb"; //ran
     
-    //tao match de check user;
-    char match[] = "AvB";
+    int result = compare(j, s); //work in function w/ return
     
-    int results = result(user, match);
+    printf("%d", result); //result
     
-    //ans 
-    printf("%d\n", results);
+    
     return 0;
     }
