33918263d4a0ba83a8d454c7a200c263cde82a81
6C2P4A118680823 <chanhnguyencute12@gmail.com>
2024-11-05 23:48:09 +0700

    more readable

--- 56.c
+++ 56.c
@@ -1,60 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//tao struct de tao arr
 typedef struct {
-    int start; //so dau
-    int end; //so sau
-    }hashmap_Intervals;
-    
-//ham sap xep
-int compare (const void* a, const void* b){
-    hashmap_Intervals* intervalA = (hashmap_Intervals*)a; //chuyen tham chieu gia tri sang intervalA qua hashmap_Intervals
-    hashmap_Intervals* intervalB = (hashmap_Intervals*)b;//nhu tren
-    return intervalA->start - intervalB->start;    
-    }
-    
-//tao ham merge return 2 bien arr va return size;
-hashmap_Intervals *merge(hashmap_Intervals* interval, int interval_size, int *returnsize){
-    //neu ko co size == erorr;
-    if (interval_size == 0){
-        *returnsize = 0;
-        return NULL;
-        }
-    //tao results
-    hashmap_Intervals *results = (hashmap_Intervals*) malloc(interval_size * sizeof(hashmap_Intervals));
-    //sap xep mang
-    qsort(interval, interval_size, sizeof(hashmap_Intervals), compare);
-    int index = 0; //bang moi gia giu so dau tien
-    results[index] = interval[0]; //so dau tien
-    //tao loop de check
-    for (size_t i = 0; i < interval_size; i++){
-        if(results[index].end >= interval[i].start){
-            results[index].end = results[index].end > interval[i].end ? results[index].end : interval[i].end;
-            } else {
-                index++;
-                results[index] = interval[i];
-                }
-        }
-    *returnsize = index + 1;
-    return results;
-    }
+    int start;
+    int end;
+}interval;
 
-int main(){
+int compare (const int *a, const int *b){
     
-    //cho mang tu struct;
-    hashmap_Intervals interval[] = {{1, 2}, {1, 1}};
-    //tinh mang
-    int interval_size = sizeof(interval) / sizeof(interval[0]);
-    //in ra thong qua return  size;
-    int returnSize;
-    //hoat dong trong mang
-    hashmap_Intervals *mergeInterval = merge(interval, interval_size, &returnSize);
+    interval *A = (interval*)a;
+    interval *B = (interval*)b;
     
-    //in ra
-    for (size_t i = 0; i < returnSize; i++){
-     printf("[%d, %d]", mergeInterval[i].start, mergeInterval[i].end);
-     }
-     
-    return 0;
+    return A - B;
+}
+
+interval *merge(interval *arr_nums, int size, int *returnSize){
+    
+    interval *result = (interval*) malloc (size * sizeof(interval));
+    int index = 0; 
+    result[index] = arr_nums[0];
+    
+    qsort(arr_nums, size, sizeof(interval), compare); //quick sort
+    
+    for(size_t i = 0; i < size; i++){
+        if(result[index].end >= arr_nums[i].start){
+            result[index].end = result[index].end > arr_nums[i].end ? result[index].end : arr_nums[i].end;
+        } else {
+            index++;
+            result[index] = arr_nums[i];
+        }
     }
+    
+    *returnSize = index + 1;
+    
+    return result;
+}
+
+int main() {
+    
+    interval arr_nums[] = {{1, 3}, {2, 2}, {9, 10}}; //[1, 3] [9, 10]
+    
+    int size = sizeof(arr_nums) / sizeof(arr_nums[0]);
+    int returnSize; //size for print
+    
+    interval *mergeInterval = merge(arr_nums, size, &returnSize);
+    
+    for (size_t i = 0; i < returnSize; i++){
+        printf("%d -> %d\n", mergeInterval[i].start, mergeInterval[i].end);
+    }
+    
+    free(mergeInterval); //free all
+    
+    return 0;
+}
